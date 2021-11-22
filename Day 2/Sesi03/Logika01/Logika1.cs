e022e72a01cc66e6fbada3b131eceadc416e61e0
henchhh <d34ndri06@gmail.com>
2021-11-23 06:47:08 +0700

    Hactiv8 C# Sesi03

---
+++ Day 2/Sesi03/Logika01/Logika1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Logika01
+{
+    public class Logika1{
+        public static void Main(string[] args)
+        {
+            int a = 92;
+            int b = 53;
+
+            if (a>b)
+            {
+                Console.WriteLine("a lebih besar dari b");
+            }
+        }
+    }
+}
---
+++ Day 2/Sesi03/Logika07/Logika7.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Logika04
+{
+    public class Logika4{
+        public static void Main(string[] args)
+        {
+            for(int a=10; a<20; a++)
+            {
+                Console.WriteLine("Iterasi ke - "+a);
+            }
+        }
+    }
+}
