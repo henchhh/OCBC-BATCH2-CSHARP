25e813fecdde2c617d5bc8b23687d79e772f7f92
henchhh <d34ndri06@gmail.com>
2021-11-24 10:38:17 +0700

    Sesi05

---
+++ Day 3/Sesi05/Encapsulation01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Encapsulation01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pesawat pesawat = new Pesawat();
+            
+            pesawat.nama = "GARUDAKU";
+            pesawat.ketinggian = "2500 kaki";
+
+            pesawat.terbang();
+            pesawat.sudahterbang();
+        }
+    }
+}
---
+++ Day 3/Sesi05/Inheritance01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inheritance01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            pesawat pesawat = new pesawat();
+            pesawat_tempur pswttempur = new pesawat_tempur();
+            
+            pesawat.Nama = "Helly";
+            pesawat.JumlahRoda = 3;
+            pesawat.Ketinggian = "1000 kaki";
+            pesawat.JumlahPenumpang = "3 penumpang";
+            pswttempur.Nama = "AZ500TPU";
+            pswttempur.JumlahRoda = 5;
+            pswttempur.Ketinggian = "200 kaki";
+            pswttempur.JumlahPenumpang = "2 penumpang plus kopilot dan asistance";
+
+            pesawat.terbang();
+            pswttempur.terbangtinggi();
+        }
+    }
+}
