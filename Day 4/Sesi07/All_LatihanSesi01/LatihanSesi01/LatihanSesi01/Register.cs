5fb7b5d5a3e73a91c3e8175b82e5e36bef045714
henchhhh <deandri.firdaus@gmail.com>
2021-11-25 17:44:07 +0700

    Sesi07

---
+++ Day 4/Sesi07/All_LatihanSesi01/LatihanSesi01/LatihanSesi01/Register.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatihanSesi01
+{
+    public partial class Register : Form
+    {
+        config db = new config();
+        public Register()
+        {
+            InitializeComponent();
+            db.Connect("userdata");
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegisterExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            db.Execute("insert into user_info(names,username,password) values('" + txtBoxRegisterNama.Text + "','" + txtBoxRegisterUsername.Text + "','" + txtBoxRegisterPassword.Text + "');");
+        }
+    }
+}
